a46a8acf05cd5c5da41a3e8eb761622467b11e5a
shmavonyankaren <karen.shmavonyan.2020@mail.ru>
2023-10-12 06:17:58 +0400

    StackWithArray

---
+++ StackWithArray.js
@@ -0,0 +1,31 @@
+class Stack {
+    constructor() {
+        this.items = [];
+    }
+
+    push(element) {
+        this.items.push(element);
+    }
+
+    pop() {
+        if (this.isEmpty()) {
+            return null;
+        }
+        return this.items.pop();
+    }
+
+    top() {
+        if (this.isEmpty()) {
+            return null;
+        }
+        return this.items[this.items.length - 1];
+    }
+
+    isEmpty() {
+        return this.items.length === 0;
+    }
+
+    getSize() {
+        return this.items.length;
+    }
+}
