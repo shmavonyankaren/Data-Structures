61d6b6694c2486cf4f222ed67febc6688e87c584
shmavonyankaren <karen.shmavonyan.2020@mail.ru>
2023-10-12 06:19:01 +0400

    Quaue

---
+++ Quaue.js
@@ -0,0 +1,32 @@
+class Queue {
+    constructor() {
+        this.items = [];
+    }
+
+    enqueue(element) {
+        this.items.push(element);
+    }
+
+    dequeue() {
+        if (this.isEmpty()) {
+            return null;
+        }
+        return this.items.shift();
+    }
+
+    front() {
+        return this.isEmpty() ? null : this.items[0];
+    }
+
+    isEmpty() {
+        return this.items.length === 0;
+    }
+
+    size() {
+        return this.items.length;
+    }
+
+    print() {
+        console.log(this.items);
+    }
+}
